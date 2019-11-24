ee3dd1b8aec05bc5ab392a4df611709d848642f2
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-11-25 00:07:40 +0800

    Update evaluateSeedSet.py

--- evaluateSeedSet.py
+++ evaluateSeedSet.py
@@ -5,11 +5,20 @@ sc_option_seq = [1, 2, 3]
 ds_option_seq = [1, 2]
 cm_seq = [1, 2]
 prod_seq = [1, 2]
-model_seq = ['mbcs', 'mbcsd', 'mbcsM', 'mbcsMd',
-             'mmioa', 'mmioad', 'mmioar', 'mmioard',
-             'mdag1', 'mdag1d', 'mdag1r', 'mdag1rd',
-             'mdag2', 'mdag2d', 'mdag2r', 'mdag2rd',
-             'mng', 'mngd', 'mngr', 'mngrd', 'mhd', 'mr']
+wd_seq = [1, 2, 3]
+model_seq = ['mmioaepw', 'mmioadepw', 'mmioarepw', 'mmioardepw',
+             'mdag1epw', 'mdag1depw', 'mdag1repw', 'mdag1rdepw',
+             'mdag2epw', 'mdag2depw', 'mdag2repw', 'mdag2rdepw',
+             'mbcs', 'mbcsd', 'mbcsM', 'mbcsMd',
+             'mmioa', 'mmioad', 'mmioapw', 'mmioadpw',
+             'mmioar', 'mmioard', 'mmioarpw', 'mmioardpw',
+             'mdag1', 'mdag1d', 'mdag1pw', 'mdag1dpw',
+             'mdag1r', 'mdag1rd', 'mdag1rpw', 'mdag1rdpw',
+             'mdag2', 'mdag2d', 'mdag2pw', 'mdag2dpw',
+             'mdag2r', 'mdag2rd', 'mdag2rpw', 'mdag2rdpw',
+             'mng', 'mngd', 'mngpw', 'mngdpw',
+             'mngr', 'mngrd', 'mngrpw', 'mngrdpw',
+             'mhd', 'mr']
 
 for data_setting in dataset_seq:
     dataset_name = 'email' * (data_setting == 1) + 'dnc_email' * (data_setting == 2) + \
@@ -25,42 +34,44 @@ for data_setting in dataset_seq:
                 for prod_setting in prod_seq:
                     product_name = 'item_lphc' * (prod_setting == 1) + 'item_hplc' * (prod_setting == 2)
                     new_product_name = 'lphc' * (prod_setting == 1) + 'hplc' * (prod_setting == 2)
-                    for model_name in model_seq:
-                        for bi in range(10, 6, -1):
+                    for wd in wd_seq:
+                        wallet_distribution_type = 'm50e25' * (wd == 1) + 'm99e96' * (wd == 2) + 'm66e34' * (wd == 3)
+                        for model_name in model_seq:
+                            for bi in range(10, 6, -1):
 
-                            path0 = 'result/' + new_dataset_name + '_' + cascade_model + '_' + seed_cost_option
-                            path = path0 + '/' + model_name + '_ds' * diff_seed_option
-                            result_name = path + '/m50e25_' + new_product_name + '_bi' + str(bi) + '.txt'
+                                path0 = 'result/' + new_dataset_name + '_' + cascade_model + '_' + seed_cost_option
+                                path = path0 + '/' + model_name + '_ds' * diff_seed_option
+                                result_name = path + '/' + wallet_distribution_type + '_' + new_product_name + '_bi' + str(bi) + '.txt'
 
-                            try:
-                                ss_time = 0.0
-                                s_set = []
-                                with open(result_name) as f:
-                                    for lnum, line in enumerate(f):
-                                        if lnum == 5:
-                                            (l) = line.split()
-                                            ss_time = float(l[-1])
-                                            # print(ss_time)
-                                        if lnum == 12:
-                                            (l) = line.split()
-                                            # print(l)
-                                            for l_item in l[14:]:
-                                                l_item = l_item.replace(',', '')
-                                                l_item = l_item.replace('\'', '')
-                                                l_item = l_item.replace('}', '')
-                                                l_item = l_item.replace('[', '')
-                                                l_item = l_item.replace(']', '')
-                                                # print(l_item)
-                                                if 'set()' in l_item:
-                                                    s_set.append(set())
-                                                elif '{' in l_item:
-                                                    l_item = l_item.replace('{', '')
-                                                    s_set.append(set())
-                                                    s_set[-1].add(l_item)
-                                                else:
-                                                    s_set[-1].add(l_item)
-                                print(s_set)
-                                evaM = EvaluationM(model_name, dataset_name, product_name, seed_cost_option, diff_seed_option, cascade_model)
-                                evaM.evaluate(bi, 'm66e34', s_set, ss_time)
-                            except FileNotFoundError:
-                                continue
+                                try:
+                                    ss_time = 0.0
+                                    s_set = []
+                                    with open(result_name) as f:
+                                        for lnum, line in enumerate(f):
+                                            if lnum == 2:
+                                                (l) = line.split()
+                                                ss_time = float(l[-1])
+                                                # print(ss_time)
+                                            if lnum == 12:
+                                                (l) = line.split()
+                                                # print(l)
+                                                for l_item in l:
+                                                    l_item = l_item.replace(',', '')
+                                                    l_item = l_item.replace('\'', '')
+                                                    l_item = l_item.replace('}', '')
+                                                    l_item = l_item.replace('[', '')
+                                                    l_item = l_item.replace(']', '')
+                                                    # print(l_item)
+                                                    if 'set()' in l_item:
+                                                        s_set.append(set())
+                                                    elif '{' in l_item:
+                                                        l_item = l_item.replace('{', '')
+                                                        s_set.append(set())
+                                                        s_set[-1].add(l_item)
+                                                    else:
+                                                        s_set[-1].add(l_item)
+                                    # print(s_set)
+                                    evaM = EvaluationM(model_name, dataset_name, product_name, seed_cost_option, diff_seed_option, cascade_model)
+                                    evaM.evaluate(bi, wallet_distribution_type, s_set, ss_time)
+                                except FileNotFoundError:
+                                    continue
