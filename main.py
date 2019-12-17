a20a53227d6b4a41f801349a84b6edde8837c864
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-12-17 17:19:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
     dataset_seq = [1, 2, 3, 4]
     sc_option_seq = [1, 2, 3]
     cm_seq = [1, 2]
-    ds_option_seq = [1, 2]
     prod_seq = [1, 2]
     wd_seq = [1, 2, 3]
 
@@ -15,62 +14,35 @@ if __name__ == '__main__':
             seed_cost_option = 'dp' * (sc_option == 1) + 'd' * (sc_option == 2) + 'p' * (sc_option == 3)
             for cm in cm_seq:
                 cascade_model = 'ic' * (cm == 1) + 'wc' * (cm == 2)
-                for ds_option in ds_option_seq:
-                    diff_seed_option = False if ds_option == 1 else True
-                    for prod_setting in prod_seq:
-                        product_name = 'item_lphc' * (prod_setting == 1) + 'item_hplc' * (prod_setting == 2)
+                for prod_setting in prod_seq:
+                    product_name = 'item_lphc' * (prod_setting == 1) + 'item_hplc' * (prod_setting == 2)
 
-                        # Model('mmioa', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_mioa(r_flag=False)
-                        # Model('mmioar', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_mioa(r_flag=True)
-                        # Model('mdag1', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag1(r_flag=False)
-                        # Model('mdag1r', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag1(r_flag=True)
-                        # Model('mdag2', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag2(r_flag=False)
-                        # Model('mdag2r', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag2(r_flag=True)
-                        # Model('mng', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_ng(r_flag=False)
-                        # Model('mngr', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_ng(r_flag=True)
-                        # Model('mbcs', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_bcs()
-                        # Model('mhd', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_hd()
-                        # Model('mr', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_r()
+                    # Model('mmioa', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_mioa(r_flag=False)
+                    # Model('mmioar', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_mioa(r_flag=True)
+                    # Model('mdag1', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag1(r_flag=False)
+                    # Model('mdag1r', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag1(r_flag=True)
+                    # Model('mdag2', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag2(r_flag=False)
+                    # Model('mdag2r', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag2(r_flag=True)
+                    # Model('mng', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_ng(r_flag=False)
+                    # Model('mngr', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_ng(r_flag=True)
+                    # Model('mbcs', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_bcs()
+                    # Model('mhd', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_hd()
+                    # Model('mr', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_r()
 
-                        # Model('mmioad', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_mioa(r_flag=False, d_flag=True)
-                        # Model('mmioard', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_mioa(r_flag=True, d_flag=True)
-                        # Model('mdag1d', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag1(r_flag=False, d_flag=True)
-                        # Model('mdag1rd', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag1(r_flag=True, d_flag=True)
-                        # Model('mdag2d', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag2(r_flag=False, d_flag=True)
-                        # Model('mdag2rd', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_dag2(r_flag=True, d_flag=True)
-                        # Model('mngd', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_ng(r_flag=False, d_flag=True)
-                        # Model('mngrd', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_ng(r_flag=True, d_flag=True)
-                        # Model('mbcsd', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option).model_bcs(d_flag=True)
+                    for wd in wd_seq:
+                        wallet_distribution_type = 'm50e25' * (wd == 1) + 'm99e96' * (wd == 2) + 'm66e34' * (wd == 3)
 
-                        for wd in wd_seq:
-                            wallet_distribution_type = 'm50e25' * (wd == 1) + 'm99e96' * (wd == 2) + 'm66e34' * (wd == 3)
-
-                            # Model('mmioaepw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=False, epw_flag=True)
-                            # Model('mmioarepw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=True, epw_flag=True)
-                            # Model('mdag1epw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=False, epw_flag=True)
-                            # Model('mdag1repw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=True, epw_flag=True)
-                            # Model('mdag2epw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=False, epw_flag=True)
-                            # Model('mdag2repw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=True, epw_flag=True)
-                            # Model('mmioapw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=False)
-                            # Model('mmioarpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=True)
-                            # Model('mdag1pw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=False)
-                            # Model('mdag1rpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=True)
-                            # Model('mdag2pw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=False)
-                            # Model('mdag2rpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=True)
-                            # Model('mngpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_ng(r_flag=False)
-                            # Model('mngrpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_ng(r_flag=True)
-
-                            # Model('mmioadepw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=False, d_flag=True, epw_flag=True)
-                            # Model('mmioardepw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=True, d_flag=True, epw_flag=True)
-                            # Model('mdag1depw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=False, d_flag=True, epw_flag=True)
-                            # Model('mdag1rdepw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=True, d_flag=True, epw_flag=True)
-                            # Model('mdag2depw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=False, d_flag=True, epw_flag=True)
-                            # Model('mdag2rdepw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=True, d_flag=True, epw_flag=True)
-                            # Model('mmioadpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=False, d_flag=True)
-                            # Model('mmioardpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=True, d_flag=True)
-                            # Model('mdag1dpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=False, d_flag=True)
-                            # Model('mdag1rdpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=True, d_flag=True)
-                            # Model('mdag2dpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=False, d_flag=True)
-                            # Model('mdag2rdpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=True, d_flag=True)
-                            # Model('mngdpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_ng(r_flag=False, d_flag=True)
-                            # Model('mngrdpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_ng(r_flag=True, d_flag=True)
+                        # Model('mmioaepw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=False, epw_flag=True)
+                        # Model('mmioarepw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=True, epw_flag=True)
+                        # Model('mdag1epw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=False, epw_flag=True)
+                        # Model('mdag1repw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=True, epw_flag=True)
+                        # Model('mdag2epw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=False, epw_flag=True)
+                        # Model('mdag2repw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=True, epw_flag=True)
+                        # Model('mmioapw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=False)
+                        # Model('mmioarpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_mioa(r_flag=True)
+                        # Model('mdag1pw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=False)
+                        # Model('mdag1rpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag1(r_flag=True)
+                        # Model('mdag2pw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=False)
+                        # Model('mdag2rpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_dag2(r_flag=True)
+                        # Model('mngpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_ng(r_flag=False)
+                        # Model('mngrpw', dataset_name, product_name, cascade_model, seed_cost_option, diff_seed_option, wallet_distribution_type).model_ng(r_flag=True)
